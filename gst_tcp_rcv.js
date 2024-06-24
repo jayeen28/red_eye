430b6566e3030f887db46adfed3c8f860ef7447e
jayeen28 <jayeen6783@gmail.com>
2024-06-24 21:04:58 +0600

    organized stucture added

--- gst_tcp_rcv.js
+++ gst_tcp_rcv.js
@@ -1,10 +1,15 @@
 const net = require('net');
 const path = require('path');
+const fs = require('fs');
 const get_bd_date_parts = require('./get_bd_date_parts');
+
+
 const script_path = path.resolve();
-const [year, month, date, hour] = get_bd_date_parts();
-const VID_DIR = 'videos';
-const vid_path = `${year}-${month}-${date}-${hour}.mp4`;
+const [year, month, date, hour, min, sec] = get_bd_date_parts();
+const VID_DIR = path.join(script_path, 'videos', String(year), String(month), String(date));
+const vid_filename = `${year}-${month}-${date}-${hour}-${min}-${sec}.mp4`;
+if (!fs.existsSync(VID_DIR)) fs.mkdirSync(VID_DIR, { recursive: true });
+
 const gst_tcp_host = '127.0.0.1';
 const gst_tcp_port = 10000;
 
@@ -23,18 +28,16 @@ const ffmpegProcess = ffmpeg(`tcp://${gst_tcp_host}:${gst_tcp_port}`)
     .on('error', (err) => {
         console.error('Error processing stream: ' + err.message);
     })
-    .save(path.join(script_path, VID_DIR, vid_path));
-
-// target.on('data', (data) => {
-//     write_stream.write(data.toString(''), () => { })
-// });
+    .save(path.join(VID_DIR, vid_filename));
 
 // Event listener for the target connection ending
 target.on('end', () => {
     console.log('disconnected from target');
-    // write_stream.end();
-    ffmpegProcess.kill('SIGINT');
 });
 
-process.on('SIGTERM', () => target.end());
-process.on('SIGINT', () => target.end());
+function cleaner() {
+    target.end();
+    ffmpegProcess.kill('SIGINT');
+}
+
+process.on('SIGINT', () => cleaner());
